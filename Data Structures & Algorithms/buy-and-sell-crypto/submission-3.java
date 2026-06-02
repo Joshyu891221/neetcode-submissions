18314bb004e0157f07b4b6bb2465590b77dc0e6f
Joshyu <104907906+Joshyu891221@users.noreply.github.com>
2026-06-02 17:04:46 -0500

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2){ return 0;}
+        int[] pair = new int[2];
+        pair[0] = prices[0];
+        int max_profit = 0;
+        for (int i = 1; i < prices.length; i++){
+            if( prices[i] < pair[0]){
+                pair[0] = prices[i];
+                pair[1] = 0;
+                // System.out.println("buy price is " + String.valueOf(pair[0]));
+            }else if (prices[i] > pair[1]){
+                pair[1] = prices[i];
+                // System.out.println("sell price is " + String.valueOf(pair[1]));
+                max_profit = Math.max(max_profit, pair[1] - pair[0]);
+            }
+        }
+        return max_profit;
+    }
+}
