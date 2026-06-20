736c7054b2d050330f99a7d788d6ae507f2f6409
Joshyu <104907906+Joshyu891221@users.noreply.github.com>
2026-06-20 15:03:18 -0500

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        boolean isNeg = false;
+        double res = 1.0;
+        if(n < 0){
+            isNeg = true;
+            n *= -1;
+        }
+
+        while(n > 0){
+            if(n % 2 == 1) res *= x;
+            
+            x *= x;
+            n /= 2;
+        }
+
+        return isNeg ? 1 / res : res;
+
+
+    }
+}
