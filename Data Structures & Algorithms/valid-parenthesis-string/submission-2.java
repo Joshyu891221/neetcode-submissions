f150d91e0e69b6e19ca6aeb2741f9a8a1010eda7
Joshyu <104907906+Joshyu891221@users.noreply.github.com>
2026-06-24 22:29:30 -0500

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Deque<Integer> leftStack = new ArrayDeque<>();
+        Deque<Integer> starStack = new ArrayDeque<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                leftStack.offerLast(i);
+            }else if(c == '*'){
+                starStack.offerLast(i);
+            }else{
+                if(!leftStack.isEmpty()){
+                    leftStack.pollLast();
+                }else if(!starStack.isEmpty()){
+                    starStack.pollLast();
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        while(!starStack.isEmpty()){
+            if(leftStack.isEmpty()) return true;
+
+            if(leftStack.peekLast() > starStack.peekLast()){
+                return false;
+            }
+            
+            leftStack.pollLast();
+            starStack.pollLast();
+        }
+
+        return leftStack.isEmpty();
+    }
+}
